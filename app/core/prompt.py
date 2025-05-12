3489d4a046a07d714e4bfb46f28f0a1f3de1fa6a
Muninn <ruihua@crestal.network>
2025-05-12 20:07:34 +0800

    fix: try to remove cdp extra prompt

--- app/core/prompt.py
+++ app/core/prompt.py
@@ -29,26 +29,10 @@ def agent_prompt(agent: Agent, agent_data: AgentData) -> str:
         if agent_data.telegram_name:
             prompt += f"Your telegram bot name is {agent_data.telegram_name}.\n"
         # CDP
-        if (
-            agent_data.cdp_wallet_data
-            and agent.skills
-            and "cdp" in agent.skills
-            and agent.skills["cdp"].get("enabled")
-        ):
+        if agent_data.cdp_wallet_data:
             network_id = agent.network_id or agent.cdp_network_id
             wallet_data = json.loads(agent_data.cdp_wallet_data)
             prompt += f"Your wallet address in {network_id} is {wallet_data['default_address_id']} .\n"
-            prompt += (
-                "## Crypto Wallet\n\n"
-                "Wallet addresses are public information. If someone asks for your default wallet, current wallet, "
-                "personal wallet, crypto wallet, or wallet public address, don't use any address in message history, "
-                "you must use the 'get_wallet_details' tool to retrieve your wallet address every time."
-            )
-            prompt += "When you get balance from tools, or pass amount to tools, don't forget they have decimals.\n"
-            prompt += "USDC and USDT has 6 decimals, you must divide the amount you get by 10^6, multiply 10^6 when passing to tools.\n"
-            prompt += "Other currencies include native ETH usually has 18 decimals, you need divide or multiply 10^18.\n"
-            if network_id == "base-mainnet":
-                prompt += "The USDC contract address in base-mainnet is 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913\n"
     prompt += "\n"
     if agent.purpose:
         prompt += f"## Purpose\n\n{agent.purpose}\n\n"
